b91c6e380edcaf63b3bd6a63b6422441697d105f
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-06-13 15:13:56 +0200

    Add files via upload

--- installer.bat
+++ installer.bat
@@ -4,5 +4,7 @@ cd scripts
 curl --output app.js https://cuzimbisonratte.github.io/ytDownloader/app.js
 curl --output package-lock.json https://cuzimbisonratte.github.io/ytDownloader/package-lock.json
 curl --output package.json https://cuzimbisonratte.github.io/ytDownloader/package.json
+npm i
 cd ..
 del /f installer.bat
+pause
